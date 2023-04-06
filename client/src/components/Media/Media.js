676edab966b64bd68f237ad6c4873e5462b287fd
koyamot <koyamot@gmail.com>
2023-04-05 22:16:11 -0700

    fix media page

--- client/src/components/Media/Media.js
+++ client/src/components/Media/Media.js
@@ -7,16 +7,15 @@ const Media = () => {
   return (
     <div className="media-div">
       <section className="video-box">
-        <h2>Our Story:</h2>
-        <ReactPlayer
-          className="video"
-          url="https://youtu.be/91D3_5H5Sz0"
-          controls="true"
-        />
-      </section>
-      <section className="links-box">
-        <h2>Read More</h2>
-
+        <h2>Watch Our Story:</h2>
+        <div className="player-wrapper">
+          <ReactPlayer
+            className="react-player"
+            url="https://youtu.be/91D3_5H5Sz0"
+            width="100%"
+            height="100%"
+          />
+        </div>
       </section>
     </div>
   );
